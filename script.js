b84edb8cc46ea14a7d8948f1a21848ab79b2297a
Bisan09 <147491062+Bisan09@users.noreply.github.com>
2025-04-01 11:56:01 +0200

    Ejercicio 7

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ const pi = 3.14;
 /*Ejercicio 5*/
 
 numero = 54;
+
+/*Ejercicio 6*/
+
+console.log(numero);
+var numero = 10;
+
+/*Ejercicio 7*/
+
+console.log("Número:", numero);
+console.log("Palabra:", palabra);
+console.log("Respuesta:", respuesta);
+console.log("Pi:", pi);
+
